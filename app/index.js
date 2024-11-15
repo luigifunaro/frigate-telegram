386357be045ad2253f9d413daec621ebb627dcf9
lunahome <luigifunaro8@gmail.com>
2024-11-15 10:55:01 +0100

    Aggiunto meccanismo di retry nel caso di mancato avvio del polling

--- app/index.js
+++ app/index.js
@@ -13,20 +13,31 @@ const pollEventsWithErrorHandling = () => {
     }
 };
 
-const startPolling = () => {
-    processFrigateStatus()
-        .then((status) => {
+const startPolling = async () => {
+    let retries = 5; // Numero massimo di tentativi
+    const retryInterval = 10000; // Intervallo tra i tentativi (in millisecondi)
+
+    while (retries > 0) {
+        try {
+            const status = await processFrigateStatus();
             if (status === 200) {
                 logger.info('Frigate is up and running. Starting polling...');
-
                 pollEventsWithErrorHandling();
                 setInterval(pollEventsWithErrorHandling, polling.interval * 1000);
+                return;
             }
-        })
-        .catch((error) => {
+        } catch (error) {
             logger.error('Error occurred during processFrigateStatus execution:', error);
-            process.exit(1); // Exit the application with a non-zero status code
-        });
+        }
+
+        retries--;
+        logger.warn(`Retrying... (${retries} retries left)`);
+        await new Promise((resolve) => setTimeout(resolve, retryInterval));
+    }
+
+    logger.error('Frigate did not start. Exiting...');
+    process.exit(1);
 };
+
 
 startPolling();
